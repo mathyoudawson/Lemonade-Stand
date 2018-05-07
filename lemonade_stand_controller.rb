8d822687eba22b1c9573d78b157bc0430e365f98
Matthew Dawson <matthewdawson@outlook.com>
2018-05-07 20:13:40 +1200

    Refactor main controller class to reduce number of instance variables

--- lemonade_stand_controller.rb
+++ lemonade_stand_controller.rb
@@ -1,23 +1,24 @@
 require_relative 'lemonade_stand.rb'
 
 class LemonadeStandController
+
+  #attr_accessor :game
+
   def initialize
     @game = LemonadeStand.new
   end
 
   def play_game
-    while @game.funds?
-      @game.set_market_prices
-      @game.purchase('lemon')
-      @game.purchase('sugar')
-      #@game.purchase_lemons
-      #@game.purchase_sugar
-      @game.make_lemonade
-      @game.set_lemonade_price
-      @game.generate_population
-      @game.sell_lemonade
-      @game.update_temperature
-      @game.calculate_profit
+    while game.funds?
+      game.set_market_prices
+      game.purchase('lemon')
+      game.purchase('sugar')
+      game.make_lemonade
+      game.set_lemonade_price
+      game.generate_population
+      game.sell_lemonade
+      game.update_temperature
+      game.calculate_profit
     end
   end
 end
